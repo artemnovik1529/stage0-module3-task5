e6fd1234daec225d19fe14231eaf9ded5714ad32
Artem Novik <mr.artnov@mail.ru>
2022-11-05 16:56:30 +0300

    feat: finish task

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int division = toBeDivided / 5;
+        System.out.println(division);
     }
 }
